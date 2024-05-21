14a048a8dcb2c3878f0f081a9d9643e0a851b810
Ryan Welcher <me@ryanwelcher.com>
2024-05-21 14:36:48 -0400

    Correctly access the page ID.

--- _blueprint/setup.php
+++ _blueprint/setup.php
@@ -14,13 +14,13 @@ add_action(
 			'post_title'   => '',
 			'post_content' => '<!-- wp:to-do-mvc/to-do-mvc /-->'
 		];
-		$page = wp_insert_post( $page_args );
+		$page_id = wp_insert_post( $page_args );
 
-		if ( ! is_wp_error( $page ) ) {
+		if ( ! is_wp_error( $page_id ) ) {
 			// Set the site to show the page on the front.
 			update_option( 'show_on_front', 'page' );
 
-			update_option( 'page_on_front', $page->ID );
+			update_option( 'page_on_front', $page_id );
 		}
 	}
 );
