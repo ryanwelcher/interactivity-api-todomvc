04a9363963053694794af5c4862f243038dd88fc
Ryan Welcher <me@ryanwelcher.com>
2025-11-05 11:49:22 -0500

    Add the build folder so we can make using blueprints easier.

---
+++ build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '22255545ed2bfb468bda');
---
+++ build/view.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('@wordpress/interactivity', array('id' => '@wordpress/interactivity-router', 'import' => 'dynamic')), 'version' => '4c4b5c424a5d03cfb09e', 'type' => 'module');
